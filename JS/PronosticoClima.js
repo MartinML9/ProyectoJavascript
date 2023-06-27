7c63759f1e423f0b34c3a892d14cde7ffb7995a6
“MartinML9” <maldonadomartin0805@gmail.com>
2023-06-27 19:03:34 -0300

    se reemplazan los prompts se agrega funcionalidad para leer el input se agrega funcionalidad para resetear el input en caso de invalido se agrega funcionalidad para modificar el html con la ciudad ingresada se añade estructura basica de consumo de la API del clima

--- JS/PronosticoClima.js
+++ JS/PronosticoClima.js
@@ -1,6 +1,11 @@
 function detectarNumero(cadena) {
-    for (let i = 0; i < cadena.length; i++) {
-        if (!isNaN(cadena[i])) {
+    
+    let caracterEsNumero;
+    let trimCadena = cadena.replace(" ", "");
+
+    for (let i = 0; i < trimCadena.length; i++) {
+        caracterEsNumero = !isNaN(trimCadena[i]);
+        if (caracterEsNumero) {
             return true;
         }
     }
@@ -8,13 +13,36 @@ function detectarNumero(cadena) {
 }
 
 function obtenerNombreCiudad() {
-    const inputCiudad = prompt("ciudad");
-    const nombreCiudad = inputCiudad.trim();
+    const inputCiudad = document.getElementById("ciudad");
+    const nombreCiudad = inputCiudad.value.trim();
+    inputCiudad.value = '';
     if (detectarNumero(nombreCiudad)) {
         alert("Ingrese una ciudad válida que no tenga numeros.");
         return null;
     }
     return nombreCiudad;
 }
+const boton = document.getElementById('botonFormulario');
+const elementoCiudad = document.getElementById("ElementoCiudad");
+const textoOriginal = elementoCiudad.innerHTML;
+//boton.addEventListener("click", obtenerNombreCiudad);
+boton.addEventListener("click", function() { 
+    let nombreCiudad = obtenerNombreCiudad();
+    if (nombreCiudad !== null && nombreCiudad !== ""){
+        alert("La ciudad ingresada es " + nombreCiudad);
+        elementoCiudad.innerHTML = textoOriginal;
+        elementoCiudad.innerHTML += " " + nombreCiudad; 
+        
+    }
+});
 
-alert("La ciudad ingresada es " + obtenerNombreCiudad());
+const apiKey = '';
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=San%20Juan,ar&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
